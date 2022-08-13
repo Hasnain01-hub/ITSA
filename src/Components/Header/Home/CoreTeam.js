0e49b71b1e21970373267afe0cedf4e5874357ab
Hasnain01-hub <hasnainsayyed833@gmail.com>
2022-08-13 14:50:24 +0530

    fixes

--- src/Components/Header/Home/CoreTeam.js
+++ src/Components/Header/Home/CoreTeam.js
@@ -1,57 +1,70 @@
-import React from 'react'
-import './team.css'
-import 'aos/dist/aos.css'; 
-import AOS from 'aos';
+import React from "react";
+import "./team.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
 AOS.init();
 const CoreTeam = () => {
   return (
     <>
-    <div class="container1">
-        <center><h2>Core Team 2022-2023</h2></center>
-    <div class="card-wrapper">
-      
-      <div class="card" data-aos="zoom-in">
-        
-        <div class="card-image">
-          <img src="https://image.ibb.co/dUTfmJ/profile_img.jpg" alt="profile one"/>
+      <div class="container1">
+        <center>
+          <h2>Core Team 2022-2023</h2>
+        </center>
+        <div class="topspace card-wrapper">
+          <div class="teamcard">
+            <div class="card-imageteam">
+              <img
+                src="https://image.ibb.co/dUTfmJ/profile_img.jpg"
+                alt="profile one"
+              />
+            </div>
+
+            <ul class="social-icons">
+              <li>
+                <a href="">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+
+            <div class="details">
+              <h2>
+                John Smith
+                <br />
+                <span class="job-title">UI Developer</span>
+              </h2>
+            </div>
+          </div>
+          <div class="teamcard">
+            <div class="card-imageteam">
+              <img
+                src="https://image.ibb.co/dUTfmJ/profile_img.jpg"
+                alt="profile one"
+              />
+            </div>
+
+            <ul class="social-icons">
+              <li>
+                <a href="">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+
+            <div class="details">
+              <h2>
+                John Smith
+                <br />
+                <span class="job-title">UI Developer</span>
+              </h2>
+            </div>
+          </div>
+          
         </div>
-
-      <ul class="social-icons">
-        {/* <li>
-          <a href="">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-        </li> */}
-        <li>
-          <a href="">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </li>
-        {/* <li>
-          <a href="">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li> */}
-        <li>
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-        </li>
-      </ul>
-
-      <div class="details">
-        <h2>John Smith
-          <br/>
-          <span class="job-title">UI Developer</span>
-        </h2>
+        
       </div>
-    </div>
-  </div>
-    
-    </div>
-
     </>
-  )
-}
+  );
+};
 
-export default CoreTeam
+export default CoreTeam;
--- src/Components/Header/Home/Teams.js
+++ src/Components/Header/Home/Teams.js
@@ -1,54 +1,43 @@
-import React from 'react'
-import './team.css';
+import React from "react";
+import "./team.css";
 const Teams = () => {
   return (
     <>
       <div class="container1">
-        <center><h2>Our Convener</h2></center>
-    <div class="card-wrapper">
-      
-      <div class="card">
+        <center>
+          <h2>Our Convener</h2>
+        </center>
+        <div class="topspace card-wrapper">
         
-        <div class="card-image">
-          <img src="https://image.ibb.co/dUTfmJ/profile_img.jpg" alt="profile one"/>
+          <div class="teamcard">
+            <div class="card-imageteam">
+              <img
+                src="https://image.ibb.co/dUTfmJ/profile_img.jpg"
+                alt="profile one"
+              />
+            </div>
+
+            <ul class="social-icons">
+              <li>
+                <a href="">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+
+            <div class="details">
+              <h2>
+                John Smith
+                <br />
+                <span class="job-title">UI Developer</span>
+              </h2>
+            </div>
+          </div>
+          
         </div>
-
-      <ul class="social-icons">
-        {/* <li>
-          <a href="">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-        </li> */}
-        <li>
-          <a href="">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </li>
-        {/* <li>
-          <a href="">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li> */}
-        <li>
-          <a href="">
-            <i class="fab fa-linkedin"></i>
-          </a>
-        </li>
-      </ul>
-
-      <div class="details">
-        <h2>John Smith
-          <br/>
-          <span class="job-title">UI Developer</span>
-        </h2>
       </div>
-    </div>
-  </div>
-    
-    </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Teams
+export default Teams;
